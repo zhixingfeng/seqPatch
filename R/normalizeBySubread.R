7161a06fe72c4fb1ec249798688e37c0b21ba458
Zhixing Feng <zxfeng.thu@gmail.com>
2013-04-18 00:00:13 -0700

    add trim=0.1 for normalization by subreads

--- R/normalizeBySubread.R
+++ R/normalizeBySubread.R
@@ -1,9 +1,9 @@
-normalizeBySubread <- function(alnsF)
+normalizeBySubread <- function(alnsF, trim=0.1)
 {
 	for (i in 1:length(alnsF)){
 		if (i==1000*floor(i/1000))
 			cat('normalized ',i,' subreads\r')
-		alnsF[[i]]$IPD <- alnsF[[i]]$IPD - mean(alnsF[[i]]$IPD, na.rm=TRUE)		
+		alnsF[[i]]$IPD <- alnsF[[i]]$IPD - mean(alnsF[[i]]$IPD, na.rm=TRUE, trim=trim)		
 	}
 	cat('normalized ',length(alnsF),' subreads\n')
 	return(alnsF)
