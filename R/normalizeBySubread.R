65579538ac0acf42a39fef889d9428baa73c24cb
Zhixing Feng <zxfeng.thu@gmail.com>
2013-05-28 03:16:16 -0400

    add normalizeBySubread.original

--- R/normalizeBySubread.R
+++ R/normalizeBySubread.R
@@ -9,4 +9,18 @@ normalizeBySubread <- function(alnsF, trim=0.1)
 	return(alnsF)
 }
 
+normalizeBySubread.original <- function(alnsF, trim=0.1)
+{
+        for (i in 1:length(alnsF)){
+                if (i==1000*floor(i/1000))
+                        cat('normalized ',i,' subreads\r')
+		alnsF[[i]]$IPD <- alnsF[[i]]$IPD / median(alnsF[[i]]$IPD,na.rm=TRUE)
+		
+		#IPD.log <- log(alnsF[[i]]$IPD + 0.0001)
+                #alnsF[[i]]$IPD <- exp(IPD.log - mean(IPD.log, na.rm=TRUE, trim=trim)) - 0.0001
+        }
+        cat('normalized ',length(alnsF),' subreads\n')
+        return(alnsF)
+}
+
 
