1c52b8180597c12a523045c61a70d7ffcc1ee0e8
Zhixing Feng <zxfeng.thu@gmail.com>
2013-07-26 08:11:55 -0400

    add check_hemi_mod

--- R/visulization.R
+++ R/visulization.R
@@ -1,3 +1,61 @@
+checkHemiModification <- function(genomeF.native, genomeF.ctrl=NULL, idx.pos, idx.neg, dir=NULL, is.simple=FALSE)
+{
+	if (length(idx.pos) != length(idx.neg)) 
+		stop('length of idx.pos and idx.neg should be the same.')	
+	cor.all <- numeric(length(idx.pos))
+	for (i in 1:length(idx.pos)){
+		ipd.pos <- split(genomeF.native$features$ipd_pos$Streptococcus_pneumoniae_ST556[[idx.pos[i]]],
+                	genomeF.native$features$moleculeID_pos$Streptococcus_pneumoniae_ST556[[idx.pos[i]]])
+		ipd.neg <- split(genomeF.native$features$ipd_neg$Streptococcus_pneumoniae_ST556[[idx.neg[i]]],
+                	genomeF.native$features$moleculeID_neg$Streptococcus_pneumoniae_ST556[[idx.neg[i]]])
+		mol.id.pos <- names(ipd.pos)
+		mol.id.neg <- names(ipd.neg)
+		mol.id.consensus <- intersect(mol.id.pos, mol.id.neg)
+
+		ipd.pos.avg <- sapply(ipd.pos[mol.id.consensus], mean)
+		ipd.neg.avg <- sapply(ipd.neg[mol.id.consensus], mean)
+
+		if (length(ipd.pos.avg) < 3 | length(ipd.neg.avg) < 3) next
+		cor.all[i] <- cor(ipd.pos.avg, ipd.neg.avg)
+		if (is.null(dir))
+			next
+		if (!file.exists(dir))	
+			dir.create(dir, recursive = TRUE)
+		file <- paste(dir,'/','hemi_mod_pos_',idx.pos[i],'_neg_',idx.neg[i],'.png',sep='')
+		png(file)
+		if (is.simple == TRUE){
+			plot(ipd.pos.avg, ipd.neg.avg, xlim=range(ipd.pos.avg, ipd.neg.avg), ylim=range(ipd.pos.avg, ipd.neg.avg),
+				xlab='IPD forward strand', ylab='IPD backward strand', main=paste('PCC =', round(cor.all[i],2)))
+		}else{
+			ipd.ctrl.pos <- genomeF.ctrl$features$ipd_pos$Streptococcus_pneumoniae_ST556[[idx.pos[i]]]
+			ipd.ctrl.neg <- genomeF.ctrl$features$ipd_neg$Streptococcus_pneumoniae_ST556[[idx.neg[i]]]
+			if (length(ipd.ctrl.pos) < 3 | length(ipd.ctrl.neg) < 3) next
+			den.ctrl.pos <- density(ipd.ctrl.pos)
+			den.ctrl.neg <- density(ipd.ctrl.neg)
+			den.pos <- density(ipd.pos.avg)
+			den.neg <- density(ipd.neg.avg)
+			par(mfrow=c(2,2))
+			plot(den.pos, xlim=range(ipd.pos.avg, ipd.neg.avg, ipd.ctrl.pos, ipd.ctrl.neg),
+				ylim=range(den.pos$y, den.ctrl.pos$y),
+				 main='', xlab='IPD forward strand', col='red')
+			lines(den.ctrl.pos, col='blue', lty=22)	
+			plot(1,type='n')
+			plot(ipd.pos.avg, ipd.neg.avg, xlim=range(ipd.pos.avg, ipd.neg.avg, ipd.ctrl.pos, ipd.ctrl.neg),
+				ylim=range(ipd.pos.avg, ipd.neg.avg, ipd.ctrl.pos, ipd.ctrl.neg),
+				xlab='IPD forward strand', ylab='IPD backward strand', main=paste('PCC =', round(cor.all[i],2)))
+			plot(den.neg$y, den.neg$x, ylim=range(ipd.pos.avg, ipd.neg.avg, ipd.ctrl.pos, ipd.ctrl.neg),
+				xlim=range(den.neg$y, den.ctrl.neg$y),
+				type='l', xlab ='Density',ylab='IPD backward strand', col='red')	
+			lines(den.ctrl.neg$y, den.ctrl.neg$x, col='blue', lty=22)
+		}
+		dev.off()	
+	}
+	if (is.null(dir))
+		return(cor.all)
+}
+
+
+
 plotAvgIPD.density <- function(IPD, mol.id, IPD.ctrl=NULL, mol.id.ctrl=NULL, loci=NULL,
 			 ref.genome=NULL, strand=NULL, context.hyperPara=NULL, out.file, tl='density', left.len =6, right.len = 1)
 {
